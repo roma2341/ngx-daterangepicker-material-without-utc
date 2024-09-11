53a80b52d05120867c66d8f931d1fc89949b2c29
roman <roman.zinchuk@softhousegroup.com>
2024-09-11 19:33:52 +0300

    angular and material v18 update

--- schematics/ng-add/setup-project.ts
+++ schematics/ng-add/setup-project.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { chain, Rule, Tree } from '@angular-eslint/schematics';
+import { Rule, chain, Tree } from '@angular-devkit/schematics';
 import { addModuleImportToRootModule, getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import {Schema} from './schema';
